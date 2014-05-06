47d2186857d0380e34baec6d8098f40088f5ead1
jsafrit <jsafrit@localhost>
2014-05-06 11:22:32 -0400

    Added more methods for setting and getting P/S status.

--- BkPS.py
+++ BkPS.py
@@ -68,6 +68,21 @@ class BkPS:
             print "Problem with command. Exiting..."
             sys.exit(3)
 
+    def sendCmdResp(self, cmd, ln=10):
+        if not self.isOpen:
+            print "Serial port not open. Exiting..."
+            sys.exit(3)
+
+        cmd = cmd + '\r\n'
+        self.ser.write(cmd)
+        time.sleep(.5)
+        resp = self.ser.read(ln)
+        result = self.ser.read(3)
+        if 'OK' not in result:
+            print "Problem with command. Exiting..."
+            sys.exit(3)
+        return resp
+
     def off(self):
         """Turn power supply output off."""
         self.sendCmd('SOUT1')
@@ -89,10 +104,49 @@ class BkPS:
         v = str(v)
         while len(v)<3:
             v = '0'+v
-        #print v
         assert(len(str(v))==3)
         cmd = 'VOLT' + str(v)
         self.sendCmd(cmd)
+
+    def setC(self,c):
+        """Set the power supply current.
+
+          params: c -   current in range of 0..5 inclusive
+                        truncated to 1 decimal place.
+        """
+        if c<0 or c>5:
+            print "Current out of range."
+            return
+        c = int(c * 10.0)
+        c = str(c)
+        while len(c)<3:
+            c = '0'+c
+        assert(len(str(c))==3)
+        cmd = 'CURR' + str(c)
+        self.sendCmd(cmd)
+
+    def getAll(self):
+        """Returns actual voltage, current, and mode
+        as a tuple (v, c, mode)
+        """
+        resp = self.sendCmdResp('GETD',10)
+        voltage = int(resp[:4]) / 100.0
+        current = int(resp[4:8]) / 100.0
+        mode = int(resp[8:9])
+        if mode == 1:
+            mode = 'CC'
+        else:
+            mode = 'CV'
+        return (voltage, current, mode)
+
+
+    def updateStatus(self):
+        """Gets actual P/S conditions and prints to screen"""
+        resp = self.getAll()
+        print 'Current state:'
+        print ' Voltage:   ', resp[0]
+        print ' Current:   ', resp[1]
+        print ' Mode:      ', resp[2]
 
 def main():
     comm = raw_input('Please enter connected PS comm port (i.e. COM4): ')
@@ -106,11 +160,20 @@ def main():
     time.sleep(5)
     ps.off()
 
+    print 'Setting Current to 1.2A'
+    ps.setC(1.2)
+    ps.on()
+    time.sleep(5)
+    ps.off()
+
     print 'Setting Voltage to 12V'
     ps.setV(12)
     ps.on()
     time.sleep(5)
     ps.off()
+
+    print 'Getting Status'
+    ps.updateStatus()
 
     print 'Setting Voltage to 20V'
     ps.setV(20)
